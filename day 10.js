f6b6129ab57e4f5156c2df387b129ff5414e285d
William Leung <gwilliamleung@gmail.com>
2022-12-10 13:47:50 -0500

    day 10

---
+++ day 10.js
@@ -0,0 +1,11 @@
+/* From Day 3 forward I will not be sharing the prompt as it is only for paid members of Scrimba
+please sign up for Scrimba to see the prompts
+
+https://scrimba.com/learn/javascriptmas
+
+*/
+
+function sortByLength(strs) {
+    return strs.sort((a,b) => a.length - b.length);
+}
+
